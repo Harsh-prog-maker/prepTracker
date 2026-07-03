d19e30930ad3212fbe2bf10aaf487e87bec28ddb
Harsh-prog-maker <hm2954141@gmail.com>
2026-07-03 21:15:29 +0530

    Create user model and first database write

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const userRoutes = require("./routes/userRoutes");
+
 app.get("/", (req, res) => {
-    res.send("PrepTracker Backend is Running 🚀");
+  res.send("PrepTracker Backend is Running 🚀");
 });
+
+app.use("/api/users", userRoutes);
 
 module.exports = app;
---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require("../models/User");
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create({
+      name: "Harsh",
+      email: "harsh@gmail.com",
+      password: "123456",
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createUser };
