66b8ca080e78bfa312c4d22ff63251b43fa221ac
Harsh-prog-maker <hm2954141@gmail.com>
2026-07-05 01:48:50 +0530

    Configure production CORS

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://prep-tracker-three-sage.vercel.app",
+    ],
   })
 );
 
